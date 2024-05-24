83ba4b7c70388ebb3f5b9afa62739d4f3c54993c
JamilyVillasBoas <Milyvillasboas@gmail.com>
2024-05-24 17:23:51 -0300

    News

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,96 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+       /* String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+		
+//qual o resultado das expressoes abaixo?
+
+String concatenacao ="?"; 
+
+System.out.println(concatenacao);
+
+concatenacao = 1+1+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = 1+"1"+1+"1";
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+1+1+1;
+
+System.out.println(concatenacao);
+
+concatenacao = "1"+(1+1+1);
+
+System.out.println(concatenacao); */
+// ------------------------------------------------------------------------------------------------------
+
+
+// int numero = 5; 
+
+// numero ++; // ou numero = numero + 1:
+
+// System.out.println(numero);
+
+//-------------------------------------------------------------------------------------------------------
+
+// boolean variavel = true;
+
+// System.out.println(!variavel); //Temporário
+
+// variavel = !variavel; //Definitivo
+
+// System.out.println(variavel);
+
+//-------------------------------------------------------------------------------------------------------
+
+// int a, b;
+
+// a = 5;
+// b = 6;
+
+// String resultado = a==b ? "verdadeiro" : "falso";
+
+// System.out.println(resultado);
+
+//--------------------------------------------------------------------------------------------------------
+// String nomeUm = "GLEYSON";
+// String nomeDois = new String ("GLEYSON");
+
+// System.out.println(nomeUm.equals(nomeDois));
+
+
+// int numero1 = 1;
+// int numero2 = 2;
+
+// boolean simNao = numero1 == numero2;
+
+// if (numero1 < numero2){
+//     System.out.println("A nossa condição é verdadeira");
+// }
+
+// System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+//--------------------------------------------------------------------------------------------------------
+
+// boolean condicao1=true;
+
+// boolean condicao2=true;
+
+// if (condicao1 && (7>4)){
+//     System.out.println("As duas condições são verdadeiras");
+// }
+
+// if (condicao1 || condicao2){
+//     System.out.println("Uma das condições é verdadeira");
+// }
+
+//--------------------------------------------------------------------------------------------------------
+    }
+}
