be64f84aec59229cf950a4ce5ed230739b8159fa
JamilyVillasBoas <Milyvillasboas@gmail.com>
2024-05-24 22:28:07 -0300

    news

---
+++ controle-de-fluxo/src/ExemploForArrays.java
@@ -0,0 +1,15 @@
+public class ExemploForArrays {
+    public static void main(String[] args) {
+        // Em arrays o indice inicia em ZERO
+        String alunos [] = {"FELIPE","JONAS","JULIA","MARCOS"};
+        for (int x=0; x < alunos.length; x++){
+            System.out.println("O aluno no indice x="+ x +"é" + alunos [x]);
+        }
+
+        // Forma Abreviada
+
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
