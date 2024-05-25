b387768fd7429781a8d80e6c784d239fdd112b68
JamilyVillasBoas <Milyvillasboas@gmail.com>
2024-05-24 21:37:08 -0300

    news

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,61 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+       // Controle de fluxo Simples
+        // double saldo = 25.0;
+        // double valorSolicitado = 17.0;
+ 
+        // if(valorSolicitado < saldo)
+        //  saldo = saldo - valorSolicitado;
+ 
+        //  System.out.println(saldo);
+
+        //-------------------------------------------------------------------------------------------------
+
+        //Controle de fluxo Composta
+
+        // Double saldo = 25.0;
+        // double valorSolicitado = 22.0;
+       
+        // if(valorSolicitado < saldo){
+        //  saldo = saldo - valorSolicitado;
+        //  System.out.println("Novo saldo :" + saldo);
+ 
+        // }else
+        //  System.out.println("Saldo insuficiente");
+
+        //----------------------------------------------------------------------------------------------------
+
+        // Condicional Encadeada
+
+
+        // if ()
+        // System.out.println();
+
+        // else if()
+        // System.out.println();
+
+        // else if ()//true or false
+        // System.out.println();
+
+        // else()
+        // System.out.println();
+
+        //-----------------------------------------------------------------------------------------------------------------
+
+        // int nota = 6;
+
+        // // String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+        // // System.out.println(resultado);
+
+        // String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+        // System.out.println(resultado);
+
+        //------------------------------------------------------------------------------------------------------------------
+
+       
+
+
+
+
+    }
+}
